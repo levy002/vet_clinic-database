e49a6afe5297081dda5273766793aded1fb39f97
levy002 <levy.ukwi002@gmail.com>
2022-05-30 23:43:53 +0200

    fixing rows value in data table

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered,weight_kg) values(1, 'Agumon', '2020-03-02', true, 0, 10.23);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) values(2, 'Gabumon', '2018-11-15',true, 2, 8),(3, 'Pikachu', '2021-01-07',false, 1, 15.04),(4,'Devimon','2017-05-12',true, 5, 11);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered,weight_kg) values(1, 'Agumon', '2020-03-02', 0, true, 10.23);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) values(2, 'Gabumon', '2018-11-15', 2, true, 8),(3, 'Pikachu', '2021-01-07', 1, false, 15.04),(4, 'Devimon', '2017-05-12', 5, true,11);
